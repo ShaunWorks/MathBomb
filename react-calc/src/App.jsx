c7d856fadd9847c4044961247517408202e038df
Torian <tharris@zyrobotics.com>
2019-03-29 16:24:00 -0400

    formatting changes

--- react-calc/src/App.jsx
+++ react-calc/src/App.jsx
@@ -8,6 +8,17 @@ import { Bomb } from "./components/Bomb"
 import * as math from "mathjs";
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.operators = ['+', '-', '*', '/'];
+    this.state = {
+      input: '',
+      bombs: [],
+      id: 0
+    };
+  }
+
   componentDidMount() {
     document.addEventListener('keydown', this.handleKey);
     setInterval(() => {
@@ -25,128 +36,116 @@ export class App extends Component {
     else if (e.key === 'Enter' || e.key === '=')
       this.parseExpression();
     else if (e.key === 'c')
-      this.createBomb();
-    //this.clearInput();
+      this.clearInput();
     else if (e.key === 'Backspace')
       this.removeLastInput();
   }
 
-  constructor(props) {
-    super(props);
-
-    this.operators = ['+', '-', '*', '/'];
-    this.state = {
-      input: '',
-      bombs: [],
-      id: 0
-    };
+  createBomb = () => {
+    let b = this.state.bombs;
+    if (b.length < 3)
+      b.push(<Bomb key={this.state.id}></Bomb>);
+    this.setState({
+      bombs: b
+    },
+      this.setState({ id: this.state.id + 1 }))
   }
 
-createBomb = () => {
-  let b = this.state.bombs;
-  if(b.length < 3)
-  b.push(<Bomb key={this.state.id}></Bomb>);
-  this.setState({
-    bombs: b
-  },
-    this.setState({ id: this.state.id + 1 }))
-}
+  isOperator = val => this.operators.some(operator => val.includes(operator));
 
-isOperator = val => this.operators.some(operator => val.includes(operator));
+  clearInput = () => this.setState({ input: "" })
+  removeLastInput = () => this.setState({ input: this.state.input.substring(0, this.state.input.length - 1) })
 
-clearInput = () => this.setState({ input: "" })
-removeLastInput = () => this.setState({ input: this.state.input.substring(0, this.state.input.length - 1) })
+  // Adds the selected button's value to the input.
+  // Stops operators and decimal points from being added if
+  // the input is empty or the last input is not a number.
+  addToInput = val => {
+    const input = this.state.input;
+    if ((input !== "" && !isNaN((input).slice(-1))) || !isNaN(val))
+      this.setState({ input: this.state.input + val });
+  };
 
-// Adds the selected button's value to the input.
-// Stops operators and decimal points from being added if
-// the input is empty or the last input is not a number.
-addToInput = val => {
-  const input = this.state.input;
-  if ((input !== "" && !isNaN((input).slice(-1))) || !isNaN(val))
-    this.setState({ input: this.state.input + val });
-};
+  // evaluate the input return the result
+  handleEqual = val => String(Math.round((math.eval(val) + 0.00001) * 100) / 100);
 
-// evaluate the input return the result
-handleEqual = val => String(Math.round((math.eval(val) + 0.00001) * 100) / 100);
+  // returns if the input includes an operator and that it ends with a number
+  checkEval = () => {
+    const input = this.state.input;
+    if (this.isOperator(input))
+      if (!isNaN(input.slice(-1)))
+        return true;
+      else console.log("error: doesnt end with number");
+    else console.log("error: needs to include an operator");
 
-// returns if the input includes an operator and that it ends with a number
-checkEval = () => {
-  const input = this.state.input;
-  if (this.isOperator(input))
-    if (!isNaN(input.slice(-1)))
-      return true;
-    else console.log("error: doesnt end with number");
-  else console.log("error: needs to include an operator");
+    return false;
+  };
 
-  return false;
-};
-
-parseExpression = () => {
-  const input = this.state.input;
-  let ops = 0;
-  if (this.checkEval()) {
-    // loops through each char in input and check if its an operator
-    // loop starts at 1 to avoid intrepeting a negative sign as a minus operator
-    for (let i = 1; i < input.length; i++) {
-      if (this.isOperator(input.charAt(i)))
-        ops++;
-      if (ops === 2) {
-        let result = this.handleEqual(input.slice(0, i));
-        this.setState({ input: result + input.slice(i) });
-        return setTimeout(() => this.parseExpression(), 1000)
+  parseExpression = () => {
+    const input = this.state.input;
+    let ops = 0;
+    if (this.checkEval()) {
+      // loops through each char in input and check if its an operator
+      // loop starts at 1 to avoid intrepeting a negative sign as a minus operator
+      for (let i = 1; i < input.length; i++) {
+        if (this.isOperator(input.charAt(i)))
+          ops++;
+        if (ops === 2) {
+          let result = this.handleEqual(input.slice(0, i));
+          this.setState({ input: result + input.slice(i) });
+          return setTimeout(() => this.parseExpression(), 1000)
+        }
       }
+      return this.setState({ input: this.handleEqual(input) });
     }
-    return this.setState({ input: this.handleEqual(input) });
+    return console.log("not valid to parse")
   }
-  return console.log("not valid to parse")
-}
 
-render() {
-  return (
-    <div className="app">
-      <div className="game">
-        <div className="bomb-wrapper">
-          {this.state.bombs}
-        </div>
-        <div className="calc-wrapper">
-          <Input input={this.state.input} />
-          <div className="row">
-            <Button handleClick={this.addToInput}>7</Button>
-            <Button handleClick={this.addToInput}>8</Button>
-            <Button handleClick={this.addToInput}>9</Button>
-            <Button handleClick={this.addToInput}>/</Button>
+  render() {
+    return (
+      <div className="app">
+        <div className="game">
+          <div className="bomb-wrapper">
+            {this.state.bombs}
           </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>4</Button>
-            <Button handleClick={this.addToInput}>5</Button>
-            <Button handleClick={this.addToInput}>6</Button>
-            <Button handleClick={this.addToInput}>*</Button>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>1</Button>
-            <Button handleClick={this.addToInput}>2</Button>
-            <Button handleClick={this.addToInput}>3</Button>
-            <Button handleClick={this.addToInput}>-</Button>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>.</Button>
-            <Button handleClick={this.addToInput}>0</Button>
-            <Button handleClick={this.parseExpression}>=</Button>
-            <Button handleClick={this.addToInput}>+</Button>
-          </div>
-          <div className="row">
-            <ClearButton handleClear={this.clearInput}>
-              Clear
+          <div className="calc-wrapper">
+            <Input input={this.state.input} />
+            <div className="row">
+              <Button handleClick={this.addToInput}>7</Button>
+              <Button handleClick={this.addToInput}>8</Button>
+              <Button handleClick={this.addToInput}>9</Button>
+              <Button handleClick={this.addToInput}>/</Button>
+            </div>
+            <div className="row">
+              <Button handleClick={this.addToInput}>4</Button>
+              <Button handleClick={this.addToInput}>5</Button>
+              <Button handleClick={this.addToInput}>6</Button>
+              <Button handleClick={this.addToInput}>*</Button>
+            </div>
+            <div className="row">
+              <Button handleClick={this.addToInput}>1</Button>
+              <Button handleClick={this.addToInput}>2</Button>
+              <Button handleClick={this.addToInput}>3</Button>
+              <Button handleClick={this.addToInput}>-</Button>
+            </div>
+            <div className="row">
+              <Button handleClick={this.addToInput}>.</Button>
+              <Button handleClick={this.addToInput}>0</Button>
+              <Button handleClick={this.parseExpression}>=</Button>
+              <Button handleClick={this.addToInput}>+</Button>
+            </div>
+            <div className="row">
+              <ClearButton handleClear={this.clearInput}>
+                Clear
             </ClearButton>
-            <BackButton handleBack={this.removeLastInput}>
-              ←
+              <BackButton handleBack={this.removeLastInput}>
+                ←
             </BackButton>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
